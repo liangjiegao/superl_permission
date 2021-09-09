664d1f2a87b3d06aead29f61ce7ae22948a49284
liangjiegao <1445808283@qq.com>
2021-09-09 10:16:18 +0800

    first commit

---
+++ demo/index.php
@@ -0,0 +1,4 @@
+<?php
+require "../vendor/autoload.php";
+$Test = new Superl\Permission\      Test();
+$Test->test(); // echo "This is my first composer package"
---
+++ src/superl/Test.php
@@ -0,0 +1,11 @@
+<?php
+//Test.php文件
+namespace Superl\Permission;
+
+class Test
+{
+    function test()
+    {
+        echo "This is my first composer package";
+    }
+}
