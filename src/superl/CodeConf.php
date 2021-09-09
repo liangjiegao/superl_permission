28d380416b4f2ffa9e414c28a5d64c14caa53d27
liangjiegao <1445808283@qq.com>
2021-09-09 14:45:38 +0800

    1.0.8

--- src/superl/CodeConf.php
+++ src/superl/CodeConf.php
@@ -16,8 +16,8 @@ class CodeConf
         $config =  array(
             self::SUCCESS               => '成功！',
             self::NOT_PERMISSION        => '您没有权限访问该接口！',
-            self::LOGIN_EXPIRE          => '您没有权限访问该接口！',
-            self::REDIS_WRITE_FAIL      => '您没有权限访问该接口！',
+            self::LOGIN_EXPIRE          => '登录过期！',
+            self::REDIS_WRITE_FAIL      => 'Redis写入失败！',
 
         );
         if (is_array($other) && count($other) > 0) {
