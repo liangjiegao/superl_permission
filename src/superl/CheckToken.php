425422d64ba39bfa73d81e79afb9fe84808599f4
liangjiegao <1445808283@qq.com>
2021-09-13 17:58:27 +0800

    new

--- src/superl/CheckToken.php
+++ src/superl/CheckToken.php
@@ -32,6 +32,8 @@ class CheckToken
 
         $response = $next($request);
 
+        $this->reductionTarget();
+
         return $response;
     }
 
@@ -48,20 +50,43 @@ class CheckToken
 
         if ($list[0] === 'mc'){
             $data = [
-                'DB_DATABASE' => 'yiyu_mcglv2',
-                'DB_USERNAME' => 'yiyu',
-                'DB_PASSWORD' => 'Yi134=&156yU',
-                'DB_HOST' => '42.192.223.252',
-                'DB_PORT'=> '3306',
-                'REDIS_HOST'=> '42.192.223.252',
-                'REDIS_PORT'=>'6379',
-                'REDIS_PASSWORD' => '66gJYuyi66',
-                'prefix'    => 'yiyu_mc_php_database_mcgl_'
+                'TEMP_DB_DATABASE'   => env('DB_DATABASE'   ),
+                'TEMP_DB_USERNAME'   => env('DB_USERNAME'   ),
+                'TEMP_DB_PASSWORD'   => env('DB_PASSWORD'   ),
+                'TEMP_DB_HOST'       => env('DB_HOST'       ),
+                'TEMP_DB_PORT'       => env('DB_PORT'       ),
+                'TEMP_REDIS_HOST'    => env('REDIS_HOST'    ),
+                'TEMP_REDIS_PORT'    => env('REDIS_PORT'    ),
+                'TEMP_REDIS_PASSWORD'=> env('REDIS_PASSWORD'),
+                'DB_DATABASE'   => env('MC_DB_DATABASE'   ),
+                'DB_USERNAME'   => env('MC_DB_USERNAME'   ),
+                'DB_PASSWORD'   => env('MC_DB_PASSWORD'   ),
+                'DB_HOST'       => env('MC_DB_HOST'       ),
+                'DB_PORT'       => env('MC_DB_PORT'       ),
+                'REDIS_HOST'    => env('MC_REDIS_HOST'    ),
+                'REDIS_PORT'    => env('MC_REDIS_PORT'    ),
+                'REDIS_PASSWORD'=> env('MC_REDIS_PASSWORD'),
+                'prefix'        => 'yiyu_mc_php_database_mcgl_',
             ];
             // 使用函数更新
             UtilsClass::modifyEnv($data);
         }
+    }
 
+    public function reductionTarget(){
+        $data = [
+            'DB_DATABASE'   => env('TEMP_DB_DATABASE'   ),
+            'DB_USERNAME'   => env('TEMP_DB_USERNAME'   ),
+            'DB_PASSWORD'   => env('TEMP_DB_PASSWORD'   ),
+            'DB_HOST'       => env('TEMP_DB_HOST'       ),
+            'DB_PORT'       => env('TEMP_DB_PORT'       ),
+            'REDIS_HOST'    => env('TEMP_REDIS_HOST'    ),
+            'REDIS_PORT'    => env('TEMP_REDIS_PORT'    ),
+            'REDIS_PASSWORD'=> env('TEMP_REDIS_PASSWORD'),
+            'prefix'        => '',
+        ];
+        // 使用函数更新
+        UtilsClass::modifyEnv($data);
     }
 
 }
