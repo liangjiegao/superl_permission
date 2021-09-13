11639be4f6c8af125c0fd4f3ab637103b1355988
liangjiegao <1445808283@qq.com>
2021-09-13 17:18:51 +0800

    new

--- src/superl/CheckToken.php
+++ src/superl/CheckToken.php
@@ -36,9 +36,31 @@ class CheckToken
     }
 
     private function checkUserToken($token){
+        $this->checkTarget($token);
         $user = LoginCache::getUserByUserToken($token);
         return $user;
     }
+    private function checkTarget($token){
+        $list = explode('_', $token);
+        if (count($list) === 1){
+            return;
+        }
 
+        if ($list[0] === 'mc'){
+            $data = [
+                'DB_DATABASE' => 'yiyu_mcglv2',
+                'DB_USERNAME' => 'yiyu',
+                'DB_PASSWORD' => 'Yi134=&156yU',
+                'DB_HOST' => '42.192.223.252',
+                'DB_PORT'=> '3306',
+                'REDIS_HOST'=> '42.192.223.252',
+                'REDIS_PORT'=>'6379',
+                'REDIS_PASSWORD' => '66gJYuyi66',
+            ];
+            // 使用函数更新
+            UtilsClass::modifyEnv($data);
+        }
+
+    }
 
 }
