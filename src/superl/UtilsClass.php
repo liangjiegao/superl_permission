9abd6d40178ef08c1b21102c8185ca145f96ae0c
liangjiegao <1445808283@qq.com>
2021-11-10 10:25:12 +0800

    git tag -a v1.6.1 getCallbackJson支持外部CodeConf

--- src/superl/UtilsClass.php
+++ src/superl/UtilsClass.php
@@ -76,9 +76,14 @@ class UtilsClass
         curl_close($curl);
         return $output;
     }
+    public static $codeConf;
 
     static function getCallbackJson($code, $other = array()){
-        return json_encode(CodeConf::getConf($code, $other), JSON_UNESCAPED_UNICODE);
+        if (empty(self::$codeConf)){
+            return json_encode(CodeConf::getConf($code, $other), JSON_UNESCAPED_UNICODE);
+        }else{
+            return json_encode(self::$codeConf::getConf($code, $other), JSON_UNESCAPED_UNICODE);
+        }
     }
     /**
      * 对象 转 数组
